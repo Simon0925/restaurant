411c295d6ca8626694f58f90804617b4748024fc
Sam <49652580+Simon0925@users.noreply.github.com>
2024-12-12 18:55:01 +0000

    first commit

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -6,9 +6,11 @@ import MenuList from "../../components/MenuList/MenuList";
 
 export default function Menu () {
     return(
-        <div className="w-full h-full flex flex-col gap-6 p-2  xl:p-0 xl:py-6 xl:pr-6 xl:overflow-y-scroll">
+        <div className="w-full h-dvh flex flex-col gap-6 p-2 xl:py-6 xl:pr-6 xl:overflow-y-scroll">
             <MenuList />
-            <Footer />
+            <div className="pb-4">
+                <Footer />
+            </div>
         </div>
     )
 }
